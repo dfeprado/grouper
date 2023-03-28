e09359e624645d88e462a447d0273d6ca5c2f772
Daniel Prado <dfe.prado@gmail.com>
2023-03-28 13:30:52 -0300

    Disable Lazy-loaded routes so the app can run offline

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GroupCanActivate } from './page/group-page/can-activate.guard';
+import { GroupPageModule } from './page/group-page/group-page.module';
+import { ListPageModule } from './page/list-page/list-page.module';
+import { MemberShufflePageModule } from './page/member-shuffle-page/member-shuffle-page.module';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./page/list-page/list-page.module').then(m => m.ListPageModule) },
-  { path: 'member-shuffle', loadChildren: () => import('./page/member-shuffle-page/member-shuffle-page.module').then(m => m.MemberShufflePageModule) },
+  { path: '', loadChildren: () => ListPageModule },
+  { path: 'member-shuffle', loadChildren: () => MemberShufflePageModule },
   { 
     path: 'groups', 
-    loadChildren: () => import('./page/group-page/group-page.module').then(m => m.GroupPageModule) ,
+    loadChildren: () => GroupPageModule ,
     canActivate: [GroupCanActivate]
   }
 ];
