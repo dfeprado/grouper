863ae3f003e97484e457030f4eb923eccd575d39
Daniel Prado <dfeprado@gmail.com>
2024-12-29 16:47:11 -0300

    Inicializa a nova versão do Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { version } from './version';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  version = `${version.major}.${version.minor}.${version.release} (${new Date(version.timestamp).toLocaleString('pt-BR', {dateStyle: 'short', timeStyle: 'short'})})`;
+  title = 'grouper';
 }
--- src/app/ui-modules/dialog/interfaces/dialog-open.interface.ts
+++
@@ -1,7 +0,0 @@
-import { Type } from "@angular/core";
-
-export interface DialogOpen<T> {
-    component: Type<any>,
-    data?: T;
-    disableDispose?: boolean;
-}
